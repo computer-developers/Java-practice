1a1094aed8f782d46d833adf261d4b38d8b1b0ec
unknown <neel.patel.2012.np@gmail.com>
2016-08-18 19:32:11 +0530

    add main file in Merge Sort

---
+++ Merge Sort with multi threading/Merge.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Merge{
+	static Scanner sc=new Scanner(System.in);
+	public static void main(String arg[]){
+		proc(20);
+	}
+	static void proc(int a){
+		try{
+
+			ArrayList<Integer> arr=new ArrayList<>(a);
+			for(int i=0;i<a;i++)
+				arr.add(sc.nextInt());
+			MSort m=new MSort(arr);
+			Thread t=new Thread(m);
+			t.run();
+			t.join();
+			for(int i=0;i<arr.size();i++)
+				System.out.println(arr.get(i));
+		}catch(InterruptedException ex){}
+	}
+}
